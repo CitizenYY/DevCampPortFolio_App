eb3708eac0236398d1400d4a84e2759b0f57758b
Yin Yué <yyue87@gmail.com>
2019-06-10 17:07:47 +0200

    create Portfolios controllers index new methods & their views

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
 end
