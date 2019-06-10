6744dd32c5bc172f656a0dbc517ce42a71d83283
Yin Yué <yyue87@gmail.com>
2019-06-10 20:59:09 +0200

    add missing line Update method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -26,6 +26,8 @@ class PortfoliosController < ApplicationController
   end
 
   def update
+    @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
