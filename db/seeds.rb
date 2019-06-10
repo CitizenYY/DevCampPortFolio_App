16e0d339fadc47bc42a60d16666e2d61af81be67
Yin Yué <yyue87@gmail.com>
2019-06-10 16:45:39 +0200

    add some seeded datas in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem ipsum dolor sit amet"
+  )
+end
+
+puts "10 blog posts created."
+
+5.times do |skill|
+  Skill.create!(
+    title: "Ruby on Rails #{skill}",
+    skill_percent: 20
+  )
+end
+
+puts "5 skills created."
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "THP-Next, Greeny ZeroWaste, Créeons la Coop",
+    main_image: "https://via.placeholder.com/300",
+    thumb_image: "https://via.placeholder.com/150"
+  )
+
+puts "9 portfolio items created."
