82b1eb64306503456d8efee589a1188ad46a4442
Yin Yué <yyue87@gmail.com>
2019-06-12 17:28:30 +0200

    create Topic model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_133754) do
+ActiveRecord::Schema.define(version: 2019_06_12_151832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 2019_06_11_133754) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "skill_percent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
