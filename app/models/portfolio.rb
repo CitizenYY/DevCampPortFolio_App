39ff55bdc3e37c1eca88742376c1813b1c59a117
Yin Yué <yyue87@gmail.com>
2019-06-13 16:59:55 +0200

    Implemented image defaults callback for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,12 @@ class Portfolio < ApplicationRecord
   def self.vuejs
     where(subtitle: 'VueJS')
   end
+
+  after_initialize :set_defaults # callback
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
+end
 end
